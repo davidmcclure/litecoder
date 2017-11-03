5be878e7a04cc1e57d346d5c2fdd1b809766be01
David McClure <davidwilliammcclure@gmail.com>
2017-11-03 14:26:23 -0400

    Stub in FreetextLocation.

--- litecoder/__init__.py
+++ litecoder/__init__.py
@@ -2,6 +2,8 @@
 
 import us
 
+from cached_property import cached_property
+
 
 class StateIndex(dict):
 
@@ -11,6 +13,21 @@ class StateIndex(dict):
         for state in us.STATES:
             self[state.abbr.lower()] = state
             self[state.name.lower()] = state
+
+
+class FreetextLocation(str):
+
+    @cached_property
+    def parts(self):
+        """Split comma-delimited parts.
+        """
+        return [p.strip() for p in self.split(',')]
+
+    @cached_property
+    def lower_parts(self):
+        """Lowercased parts.
+        """
+        return list(map(str.lower, self.parts))
 
 
 def usa_city_state(query):
