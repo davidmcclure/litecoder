7b36e1c751f6f6d5a951309c32b8baccc42dbbb6
David McClure <davidwilliammcclure@gmail.com>
2018-07-11 13:38:56 -0400

    safe_property / safe_property.cached.

--- litecoder/sources.py
+++ litecoder/sources.py
@@ -3,33 +3,23 @@
 import attr
 import os
 import ujson
+import time
 
 from collections import UserDict
 from glob import iglob
 from multiprocessing import Pool
 
 
-def try_or_log(f):
-    """Wrap a class method call in a try block. If an error is raised, return
-    None and log the exception.
-    """
-    def wrapper(*args, **kwargs):
-        try:
-            return f(*args, **kwargs)
-        except Exception as e:
-            # TODO|dev
-            # logging.exception('message')
-            return None
-    return wrapper
-
-
 class safe_property:
 
-    def __init__(self, func):
+    @classmethod
+    def cached(cls, func):
+        return cls(func, True)
+
+    def __init__(self, func, cached=False):
+        self.cached = cached
         self.__doc__ = getattr(func, "__doc__")
         self.func = func
-        self.successes = set()
-        self.fails = set()
 
     def __get__(self, obj, cls):
         """Try to set a cached property. Catch and record errors.
@@ -39,10 +29,12 @@ class safe_property:
 
         try:
             value = self.func(obj)
-            self.calls.add(obj.wof_id)
         except Exception as e:
             value = None
-            self.fails.add(obj.wof_id)
+
+        # Replace attribute with computed value.
+        if self.cached:
+            obj.__dict__[self.func.__name__] = value
 
         return value
 
@@ -64,25 +56,28 @@ class WOFLocalitiesRepo:
         with Pool(num_procs) as p:
 
             yield from p.imap_unordered(
-                WOFLocalityGeojson.from_json,
-                self.paths_iter()
+                WOFLocalityGeojson,
+                self.paths_iter(),
             )
 
 
 class WOFLocalityGeojson(UserDict):
 
-    @classmethod
-    def from_json(cls, path):
+    def __init__(self, path):
+        """Parse JSON, set path.
+        """
         with open(path) as fh:
-            return cls(ujson.load(fh))
+            super().__init__(ujson.load(fh))
+
+        self.path = path
 
     def __repr__(self):
-        return '%s<%d>' % (self.__class__.__name__, self.id)
+        return '%s<%d>' % (self.__class__.__name__, self.wof_id)
 
-    @safe_cached_property
+    @property
     def wof_id(self):
         return self['id']
 
-    @safe_cached_property
+    @safe_property
     def name(self):
         return self['properties']['name:eng_x_preferred'][0]
