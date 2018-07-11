7c6e99a9c7c9995f7765f3effc0d6e0ed01d0d5c
David McClure <davidwilliammcclure@gmail.com>
2018-07-11 16:56:53 -0400

    DB row.

---
+++ litecoder/db.py
@@ -0,0 +1,90 @@
+
+
+import os
+
+from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Text
+
+
+# TODO: Config-ify
+db_path = os.path.join(
+    os.path.dirname(__file__),
+    'data', 'litecoder.db',
+)
+
+url = URL(drivername='sqlite', database=db_path)
+engine = create_engine(url)
+factory = sessionmaker(bind=engine)
+session = scoped_session(factory)
+
+
+class BaseModel:
+
+    @classmethod
+    def reset(cls):
+        """Drop and (re-)create table.
+        """
+        cls.metadata.drop_all(engine)
+        cls.metadata.create_all(engine)
+
+
+BaseModel = declarative_base(cls=BaseModel)
+BaseModel.query = session.query_property()
+
+
+class City(BaseModel):
+
+    __tablename__ = 'city'
+
+    wof_id = Column(Integer, primary_key=True)
+
+    dbpedia_id = Column(String)
+
+    freebase_id = Column(String)
+
+    factual_id = Column(String)
+
+    fips_code = Column(Integer)
+
+    geonames_id = Column(Integer)
+
+    geoplanet_id = Column(Integer)
+
+    library_of_congress_id = Column(String)
+
+    new_york_times_id = Column(String)
+
+    quattroshapes_id = Column(Integer)
+
+    wikidata_id = Column(String)
+
+    wikipedia_page = Column(String)
+
+    name = Column(String, nullable=False)
+
+    country_iso = Column(String, nullable=False)
+
+    country_name = Column(String, nullable=False)
+
+    state_name = Column(String, nullable=False)
+
+    state_abbr = Column(String, nullable=False)
+
+    latitude = Column(Float)
+
+    longitude = Column(Float)
+
+    population = Column(Integer)
+
+    population_rank = Column(Integer)
+
+    wikipedia_wordcount = Column(Integer)
+
+    elevation = Column(Integer)
+
+    area_m2 = Column(Float)
+
+    geometry_json = Column(Text)
