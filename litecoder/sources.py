c7ab08a261f4c2ed095757c4b31228fa9ee3379e
David McClure <davidwilliammcclure@gmail.com>
2018-07-11 14:26:12 -0400

    Filling in WOF getters.

--- litecoder/sources.py
+++ litecoder/sources.py
@@ -39,6 +39,10 @@ class safe_property:
         return value
 
 
+def first(seq):
+    return next((x for x in seq if x is not None), None)
+
+
 @attr.s
 class WOFLocalitiesRepo:
 
@@ -81,3 +85,77 @@ class WOFLocalityGeojson(UserDict):
     @safe_property
     def name(self):
         return self['properties']['name:eng_x_preferred'][0]
+
+    @safe_property
+    def country_iso(self):
+        return self['properties']['iso:country']
+
+    @safe_property
+    def _qs_a0(self):
+        return self['properties']['qs:a0']
+
+    @safe_property
+    def _qs_adm0(self):
+        return self['properties']['qs:adm0']
+
+    @safe_property
+    def _ne_sov0name(self):
+        return self['properties']['ne:SOV0NAME']
+
+    @safe_property
+    def country_name(self):
+        return first((self._qs_a0, self._qs_adm0, self._ne_sov0name))
+
+    @safe_property
+    def latitude(self):
+        return self['properties']['geom:latitude']
+
+    @safe_property
+    def longitude(self):
+        return self['properties']['geom:latitude']
+
+    @safe_property
+    def _wof_population(self):
+        return self['properties']['wof:population']
+
+    @safe_property
+    def _gn_population(self):
+        return self['properties']['gn:population']
+
+    @safe_property
+    def _wk_population(self):
+        return self['properties']['wk:population']
+
+    @safe_property
+    def population(self):
+        return first((
+            self._wof_population,
+            self._gn_population,
+            self._wk_population,
+        ))
+
+    @safe_property
+    def population_rank(self):
+        return self['properties']['wof:population_rank']
+
+    @safe_property
+    def wikipedia_wordcount(self):
+        return self['properties']['wk:wordcount']
+
+    @safe_property
+    def _gn_elevation(self):
+        return self['properties']['gn:elevation']
+
+    @safe_property
+    def _ne_elevation(self):
+        return self['properties']['ne:ELEVATION']
+
+    @safe_property
+    def elevation(self):
+        return first((self._gn_elevation, self._ne_elevation))
+
+    # state
+    # area
+    # geometry
+    # ids
+    # alternate names
