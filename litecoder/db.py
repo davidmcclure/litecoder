9333b13c1c63b78139d1c931e393042bc8ec260e
David McClure <davidwilliammcclure@gmail.com>
2018-07-19 10:03:46 -0400

    Split db and models.

--- litecoder/db.py
+++ litecoder/db.py
@@ -1,22 +1,10 @@
 
 
 import os
-import us
-import pkgutil
-import yaml
-
 
 from sqlalchemy.engine.url import URL
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session, deferred
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, Text
-
-from .utils import safe_property
-
-
-ALT_NAMES = yaml.load(pkgutil.get_data('litecoder', 'data/city-alt-names.yml'))
-
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 # TODO: Config-ify
 db_path = os.path.join(
@@ -25,89 +13,9 @@ db_path = os.path.join(
 )
 
 url = URL(drivername='sqlite', database=db_path)
+
 engine = create_engine(url)
+
 factory = sessionmaker(bind=engine)
+
 session = scoped_session(factory)
-
-
-class BaseModel:
-
-    @classmethod
-    def reset(cls):
-        """Drop and (re-)create table.
-        """
-        cls.metadata.drop_all(engine)
-        cls.metadata.create_all(engine)
-
-
-BaseModel = declarative_base(cls=BaseModel)
-BaseModel.query = session.query_property()
-
-
-class City(BaseModel):
-
-    __tablename__ = 'city'
-
-    wof_id = Column(Integer, primary_key=True)
-
-    dbpedia_id = Column(String)
-
-    freebase_id = Column(String)
-
-    factual_id = Column(String)
-
-    fips_code = Column(Integer)
-
-    geonames_id = Column(Integer)
-
-    geoplanet_id = Column(Integer)
-
-    library_of_congress_id = Column(String)
-
-    new_york_times_id = Column(String)
-
-    quattroshapes_id = Column(Integer)
-
-    wikidata_id = Column(String)
-
-    wikipedia_page = Column(String)
-
-    name = Column(String, nullable=False)
-
-    country_iso = Column(String, nullable=False)
-
-    name_a0 = Column(String)
-
-    name_a1 = Column(String)
-
-    latitude = Column(Float)
-
-    longitude = Column(Float)
-
-    population = Column(Integer)
-
-    population_rank = Column(Integer)
-
-    wikipedia_wordcount = Column(Integer)
-
-    elevation = Column(Integer)
-
-    area_m2 = Column(Float)
-
-    geometry_json = deferred(Column(Text))
-
-    def __repr__(self):
-        return '%s<%s, %s, %s>' % (
-            self.__class__.__name__,
-            self.name,
-            self.name_a1,
-            self.name_a0,
-        )
-
-    @property
-    def alt_names(self):
-        return ALT_NAMES.get(self.wikidata_id, [])
-
-    @safe_property
-    def us_state_abbr(self):
-        return us.states.lookup(self.name_a1).abbr
