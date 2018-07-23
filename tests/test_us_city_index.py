ad8094051212bb020629be101344df6ff572ac83
David McClure <davidwilliammcclure@gmail.com>
2018-07-23 13:20:30 -0400

    City test harness.

---
+++ tests/test_us_city_index.py
@@ -0,0 +1,38 @@
+
+
+import pytest
+
+from litecoder.usa import USCityIndex
+
+from tests.utils import read_yaml
+
+
+@pytest.fixture(scope='module')
+def city_idx():
+    return USCityIndex.load()
+
+
+def yield_cases():
+    """Generate cases from YAML file.
+    """
+    cases = read_yaml(__file__, 'test_us_city_index.yml')
+
+    for group in cases:
+
+        queries = group['query']
+
+        if type(queries) is str:
+            queries = [queries]
+
+        for query in queries:
+            yield query, group['matches']
+
+
+@pytest.mark.parametrize('query,matches', yield_cases())
+def test_us_city_index(city_idx, query, matches):
+
+    res = city_idx[query]
+
+    ids = [r.wof_id for r in res]
+
+    assert sorted(ids) == sorted(matches)
