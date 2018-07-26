20f38ade7035f0bbf99511731a8b8301ed6bbfe8
David McClure <davidwilliammcclure@gmail.com>
2018-07-26 18:55:44 -0400

    setup.py fixes.

--- setup.py
+++ setup.py
@@ -3,15 +3,50 @@
 from setuptools import setup, find_packages
 
 
+PACKAGE_DATA = {
+    'litecoder': [
+        'data/*.db',
+        'data/*.yml',
+        'data/*.p',
+    ]
+}
+
+INSTALL_REQUIRES = [
+    'numpy',
+    'scipy',
+    'SQLAlchemy',
+    'us',
+    'boltons',
+    'cached-property',
+    'tqdm',
+    'attrs',
+    'ujson',
+    'python-box',
+    'PyYAML',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+]
+
+
 setup(
     name='litecoder',
     version='0.1.0',
-    description='Simple geocoding.',
+    description='US city + state geocoding.',
     url='https://github.com/davidmcclure/litecoder',
     license='MIT',
     author='David McClure',
     author_email='dclure@mit.edu',
+    classifiers=CLASSIFIERS,
     packages=find_packages(),
     include_package_data=True,
-    package_data={'litecoder': ['litecoder.db']},
+    install_requires=INSTALL_REQUIRES,
+    package_data=PACKAGE_DATA,
 )
