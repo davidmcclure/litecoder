d3c6bda11fbb4dfbd65c995319de4b864c87b869
David McClure <davidwilliammcclure@gmail.com>
2018-07-26 16:38:50 -0400

    Smoke test state index.

--- tests/prod_db/test_us_state_index.py
+++ tests/prod_db/test_us_state_index.py
@@ -2,6 +2,8 @@
 
 import pytest
 
+from litecoder.models import WOFRegion
+
 from tests.utils import read_yaml
 
 
@@ -22,10 +24,23 @@ def yield_cases():
 
 
 @pytest.mark.parametrize('query,matches', yield_cases())
-def test_us_state_index(state_idx, query, matches):
+def test_cases(state_idx, query, matches):
 
     res = state_idx[query]
 
     ids = [r.data.wof_id for r in res]
 
     assert sorted(ids) == sorted(matches)
+
+
+states = WOFRegion.query.filter(WOFRegion.country_iso=='US')
+
+
+@pytest.mark.parametrize('state', states)
+def test_all(state_idx, state):
+    """Smoke test N most populous cities.
+    """
+    res = state_idx[state.name]
+    res_ids = [r.data.wof_id for r in res]
+
+    assert state.wof_id in res_ids
